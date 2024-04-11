0cdc56f04e5c9bea3228f10d51766fe2e85a5197
aligezgez <141458263+aligezgez@users.noreply.github.com>
2024-04-11 16:50:17 +0100

    Add files via upload

---
+++ src/app/components/pack/pack.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pack',
+  standalone: true,
+  imports: [],
+  templateUrl: './pack.component.html',
+  styleUrl: './pack.component.css'
+})
+export class PackComponent {
+
+}
